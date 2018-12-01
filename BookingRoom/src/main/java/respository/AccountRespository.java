cbf9b49d5b3771064e9c88cac8aa4d7b73f353e5
ThuyNT <thuynt181998@gmail.com>
2018-12-01 16:03:52 +0700

    Thuy update khởi tạo trong java

---
+++ BookingRoom/src/main/java/respository/AccountRespository.java
@@ -0,0 +1,12 @@
+package respository;
+import java.util.List;
+
+//import org.springframework.data.repository.CrudRepository;
+
+import domain.Account;
+
+//public interface AccountRespository extends CrudRepository<Contact, Integer> {
+//
+//    List<Contact> findByNameContaining(String q);
+//
+//}
