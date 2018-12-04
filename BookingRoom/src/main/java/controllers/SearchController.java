f85d6d40289a48c78a6217d79bd3dd3320123cc6
Thanh Nga <ttnga912@gmail.com>
2018-12-04 15:32:49 +0700

    Nga update các file controller (đã map được các templates)

--- BookingRoom/src/main/java/controllers/SearchController.java
+++ BookingRoom/src/main/java/controllers/SearchController.java
@@ -1,13 +1,14 @@
-package controller;
+package controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class SearchController {
 	@RequestMapping("/search")
-    public String index(Model model) {
+    public String search() {
 	return "search";
     }
 }
